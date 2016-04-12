868063e4ac0b50aa5d3197858eb1836cfc06d7aa
Janne Savukoski <janne@savukoski.name>
2016-04-13 01:33:09 +0300

    Reverse syntax

--- flystick_conf_models.py
+++ flystick_conf_models.py
@@ -1,6 +1,19 @@
 import pygame.joystick
 
 
+class Ch(object):
+    # Questionable whether all this indirection is worth having the
+    # easier reverse syntax (minus-prefix) for configuration...
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, clicks):
+        return self.fn(clicks)
+
+    def __neg__(self):
+        return Ch(lambda clicks: -self.fn(clicks))
+
+
 class Joystick(object):
     def __init__(self, joy_id):
         pygame.joystick.init()
@@ -8,37 +21,40 @@ class Joystick(object):
         self._joy.init()
 
     def axis(self, axis):
-        return lambda clicks: self._joy.get_axis(axis)
+        return Ch(lambda clicks: self._joy.get_axis(axis))
 
     def button(self, button):
-        return lambda clicks: 1. if self._joy.get_button(button) else -1.
+        return Ch(lambda clicks: 1. if self._joy.get_button(button) else -1.)
 
 
-def Neg(ch):
-    return lambda clicks: -ch(clicks)
+class Switch(object):
+    def __init__(self, source):
+        pass
 
 
-
-class Crosshair(object):
-    def __init__(self, center):
-        self.center_x, self.center_y = center
-        self.x = self.y = 0
-
-    def maybe_render(self, scrollphat):
-        if not (self.x is None or self.y is None):
-            scrollphat.set_pixel(self.x, 4 - self.y, True)
-            self.x = self.y = None
+class XYDot(object):
+    def __init__(self, center_x):
+        self.center_x = center_x
+        self.x = self.y = None
 
     def horizontal(self):
         def render(value, scrollphat):
-            self.x = self.center_x + int(round(value * 2))
-            self.maybe_render(scrollphat)
+            x = self.x_pos + int(round(value * 2))
+            if self.y is None:
+                self.x = x
+            else:
+                scrollphat.set_pixel(x, 4 - self.y, True)
+                self.x = self.y = None
         return render
 
     def vertical(self):
         def render(value, scrollphat):
-            self.y = self.center_y + int(round(value * 2))
-            self.maybe_render(scrollphat)
+            y = 2 + int(round(value * 2))
+            if self.x is None:
+                self.y = y
+            else:
+                scrollphat.set_pixel(self.x, 4 - y, True)
+                self.x = self.y = None
         return render
 
 
@@ -52,18 +68,16 @@ def HPoint(center):
     return render
 
 
-def VPoint(center):
-    x, y = center
-
+def VPoint(center_x):
     def render(value, scrollphat):
-        _y = y + int(round(value * 2))
-        scrollphat.set_pixel(x, 4 - _y, True)
+        _y = 2 + int(round(value * 2))
+        scrollphat.set_pixel(center_x, 4 - _y, True)
 
     return render
 
 
-def VBar(x_pos, width=1):
-    xs = [x_pos + x for x in range(width)]
+def VBar(center_x, width=1):
+    xs = [center_x + x for x in range(width)]
 
     def render(value, scrollphat):
         height = int(round((value + 1) / 2 * 5))
@@ -76,12 +90,12 @@ def VBar(x_pos, width=1):
     return render
 
 
-def Dot(pos, size=(1, 1)):
+def Block(corner, size=(1, 1)):
     # unpack for readability
-    x_pos, y_pos = pos
+    corner_x, corner_y = corner
     x_size, y_size = size
-    xs = [x_pos + x for x in range(x_size)]
-    ys = [y_pos + y for y in range(y_size)]
+    xs = [corner_x + x for x in range(x_size)]
+    ys = [corner_y + y for y in range(y_size)]
 
     def render(value, scrollphat):
         if value >= 0:
--- flystick_config.py
+++ flystick_config.py
@@ -4,27 +4,30 @@ stick = Joystick(0)
 
 # Output (PPM) channels.
 CHANNELS = (
-    stick.axis(0), # channel 1
-    Neg(stick.axis(1)), # channel 2
-    Neg(stick.axis(2)), # channel 3
-    stick.button(0), # channel 4
-    stick.button(1), # channel 5
+    # channel 1: aileron
+    stick.axis(0),
+    # channel 2: elevator (reversed)
+    -stick.axis(1),
+    # channel 3: throttle (reversed)
+    -stick.axis(2),
+    # channel 4: flight mode; 5 states to match scrollphat vertical resolution
+    #Switch(),
 )
 
-cross = Crosshair(center=(5, 2))
+dot = XYDot(center_x=5)
 
 # Render outputs (channels). One-to-one line match to CHANNELS.
 DISPLAY = (
-    # channel 1: cross-hair horizontal line
-    cross.horizontal(),
-    # channel 2: cross-hair vertical line
-    cross.vertical(),
+    # channel 1: dot horizontal axis
+    dot.horizontal(),
+    # channel 2: dot vertical axis
+    dot.vertical(),
     # channel 3: throttle bar
-    VBar(x_pos=0, width=2),
+    VBar(center_x=0, width=2),
     # channel 4: button square blinky
-    Dot(pos=(9, 0), size=(2, 2)),
+    Block(corner=(9, 0), size=(2, 2)),
     # channel 5: button square blinky
-    Dot(pos=(9, 3), size=(2, 2)),
+    Block(corner=(9, 3), size=(2, 2)),
 )
 
 # TODO what's the range? 128? http://www.issi.com/WW/pdf/31FL3730.pdf
